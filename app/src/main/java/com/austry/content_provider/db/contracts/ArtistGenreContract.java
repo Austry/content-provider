ee9ff78f13a8f8a959958de0ddbbc1a74dbc6dd3
Austry <iliazasimov@gmail.com>
2016-08-08 16:45:52 +0300

    for demo

--- app/src/main/java/com/austry/content_provider/db/contracts/ArtistGenreContract.java
+++
@@ -1,8 +0,0 @@
-package com.austry.content_provider.db.contracts;
-
-public class ArtistGenreContract {
-    public static final String TABLE_NAME = "artist_genres";
-    public static final String COLUMN_ID = "id";
-    public static final String COLUMN_ARTIST_ID = "artist_id";
-    public static final String COLUMN_GENRE_ID = "genre_id";
-}
--- app/src/main/java/com/austry/content_provider/db/contracts/DbContract.java
+++
@@ -1,8 +0,0 @@
-package com.austry.content_provider.db.contracts;
-
-
-public class DbContract {
-
-    public final static String DB_NAME = "artists";
-    public final static int DB_VERSION = 1;
-}
